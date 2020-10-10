d402cc0c7e45c3d2b74970e7e1645abd9deec7ad
SugandhaSapra <sugzsapra@gmail.com>
2020-10-11 00:16:39 +0530

    Project Setup

---
+++ src/main/java/com/melisende/jooqgettingstarted/JooqGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.melisende.jooqgettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqGettingStartedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melisende/jooqgettingstarted/JooqGettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melisende.jooqgettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqGettingStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
